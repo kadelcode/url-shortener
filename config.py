00cbef9d12a33e821a6c532d22d70b8732badad3
Ayodele Kadiri <kadelcode@gmail.com>
2023-10-07 17:40:08 +0100

    Update config file

--- config.py
+++ config.py
@@ -4,4 +4,20 @@ class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = config('SECRET_KEY', default-secret-key')
+    SECRET_KEY = config('SECRET_KEY', 'default-secret-key')
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    TEMPLATES_AUTO_RELOAD = True
+    #TEMPLATE_FOLDER = 'templates'
+
+class TestingConfig(Config):
+    TESTING = True
