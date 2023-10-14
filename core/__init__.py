5b0a2a7a86eaedf10c97d9bd74f0941f3f9e5cce
Ayodele Kadiri <kadelcode@gmail.com>
2023-10-14 21:06:31 +0100

    Update file

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from decouple import config
+from core.models import db
+from core.auth import login_manager
 
 app = Flask(__name__)
 
@@ -10,8 +13,16 @@ app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
 
 # initialize a database connection
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
+db.init_app(app)
+
+login_manager.init_app(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+
+bootstrap = Bootstrap(app)
 
 # import other modules in the package
 from core import routes
