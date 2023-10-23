147abdb1285638f3642ee8562d7ba52efbd3f778
Ayodele Kadiri <kadelcode@gmail.com>
2023-10-23 15:17:01 +0100

    update models file

--- core/models.py
+++ core/models.py
@@ -1,8 +1,47 @@
-from core import db
+from core.db import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from core.auth import login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# User Model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+    customshorturls = db.relationship('CustomShortUrls', backref='user')
+    # custom_short_urls = db.relationship("CustomShortUrl", backref="user")
+ 
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+ 
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class ShortUrls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(500), nullable=False)
     short_id = db.Column(db.String(20), nullable=False, unique=True)
     created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
+class CustomShortUrls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    domain = db.Column(db.String(30), nullable=True)
+    back_half = db.Column(db.String(100), nullable=True)
+    short_id = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+    short_url = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
