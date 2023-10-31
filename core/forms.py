6a8a501554e608e79c705594e2fc9a4dcca4ef76
Ayodele Kadiri <kadelcode@gmail.com>
2023-11-01 00:09:31 +0100

    Add profile photo field, update validate email and validate username fields

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from .models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Regexp, Email, EqualTo
+from flask_login import current_user
 
 
 class LoginForm(FlaskForm):
@@ -30,9 +32,22 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Username already in use.')
 
 class EditProfileForm(FlaskForm):
+    profile_photo = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
         'Usernames must have only letters, numbers, dots or underscores')])
     first_name = StringField('First Name', validators=[Length(0, 64)])
     last_name = StringField('Last Name', validators=[Length(0, 64)])
     submit = SubmitField('Update Profile')
+
+    # Checks if the email already exist in database
+    def validate_email(self, field):
+        if field.data != current_user.email:
+            if User.query.filter_by(email=field.data).first():
+                raise ValidationError('Email already registered.')
+    
+    # Checks if the username already exist in database
+    def validate_username(self, field):
+        if field.data != current_user.username:
+            if User.query.filter_by(username=field.data).first():
+                raise ValidationError('Username already in use.')
