bd6d2f92cf404ee3ebb0187408e2e2b06a0f1014
Ayodele Kadiri <kadelcode@gmail.com>
2023-11-02 16:42:33 +0100

    Update JS code

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -4,12 +4,13 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-// Script for CopyIcon
-const copyTextIcon = document.querySelector(".copy-text-icon");// Icon
+window.addEventListener('load', () => {
+  // Script for CopyIcon
+  const copyTextIcon = document.querySelector(".copy-text-icon");// Icon
 
-
-copyTextIcon.addEventListener('click', () => {
-  const textToCopy = document.getElementById('short-link').value;
+  if (copyTextIcon) {
+    copyTextIcon.addEventListener('click', () => {
+    const textToCopy = document.getElementById('short-link').value;
 
   navigator.clipboard.writeText(textToCopy)
     .then(() => {
@@ -26,4 +27,7 @@ copyTextIcon.addEventListener('click', () => {
       // Something went wrong while copying the text.
       alert("Error in copying link!")
     });
+  });
+  }
+
 });
