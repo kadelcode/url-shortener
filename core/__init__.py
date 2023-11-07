e0da3f7cf7b10d5ad5af5152b9bfa09a4cc97270
Ayodele Kadiri <kadelcode@gmail.com>
2023-11-07 20:02:35 +0100

    Add mail configuration settings

--- core/__init__.py
+++ core/__init__.py
@@ -1,17 +1,34 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from decouple import config
 from core.models import db
 from core.auth import login_manager
 from core.app import app
+import dotenv
+
+# Load the environment variables from the `.env` file.
+dotenv.load_dotenv()
 
 #app = Flask(__name__)
 
 # load configuration variables from a configuration file
 # load configuration settings
 app.config.from_object(config("APP_SETTINGS"))
+
+# Mail configuration settings
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PWD')
+
+# initialize flask mail
+mail = Mail(app)
 
 # initialize a database connection
 # db = SQLAlchemy(app)
